1a91f2fd2b15a9d9792d05cd818eba09d9420199
Boris Gombarcik <boris.gombarcik@gmail.com>
2019-08-20 11:34:34 +0200

    Adding all textures and minifying imported styles.

--- index.js
+++ index.js
@@ -12,18 +12,50 @@ async function run() {
 
         try {
             const object = await vctrApi.getObjectByName("BicycleHelmet");
-            const blackTexture = { "name": "black", "map": "./Black&Lable.jpg" }
-            const blueTexture = { "name": "blue", "map": "./Blue&Lable.jpg" }
-            const blackMaterial = await vctrApi.createMaterial(blackTexture, object.material);
+
+            const whiteTexture = { "name": "white", "map": "./white.jpg" }
+            const darkRedTexture = { "name": "darkRed", "map": "./darkred.jpg" }
+            const pinkTexture = { "name": "pink", "map": "./pink.jpg" }
+            const greenTexture = { "name": "green", "map": "./green.jpg" }
+            const blueTexture = { "name": "blue", "map": "./blue.jpg" }
+            const orangeTexture = { "name": "orange", "map": "./orange.jpg" }
+            const redTexture = { "name": "red", "map": "./red.jpg" }
+
+            const whiteMaterial = await vctrApi.createMaterial(whiteTexture, object.material);
+            const darkRedMaterial = await vctrApi.createMaterial(darkRedTexture, object.material);
+            const pinkMaterial = await vctrApi.createMaterial(pinkTexture, object.material);
+            const greenMaterial = await vctrApi.createMaterial(greenTexture, object.material);
             const blueMaterial = await vctrApi.createMaterial(blueTexture, object.material);
+            const orangeMaterial = await vctrApi.createMaterial(orangeTexture, object.material);
+            const redMaterial = await vctrApi.createMaterial(redTexture, object.material);
 
             console.log(await vctrApi.getObjects());
             document.getElementById("whiteColor").addEventListener("click", () => {
-                vctrApi.setMaterial(object.name, blackMaterial.name);
+                vctrApi.setMaterial(object.name, whiteMaterial.name);
+            });
+
+            document.getElementById("darkRedColor").addEventListener("click", () => {
+              vctrApi.setMaterial(object.name, darkRedMaterial.name);
+            });
+
+            document.getElementById("pinkColor").addEventListener("click", () => {
+              vctrApi.setMaterial(object.name, pinkMaterial.name);
+            });
+
+            document.getElementById("greenColor").addEventListener("click", () => {
+              vctrApi.setMaterial(object.name, greenMaterial.name);
             });
 
             document.getElementById("blueColor").addEventListener("click", () => {
                 vctrApi.setMaterial(object.name, blueMaterial.name);
+            });
+
+            document.getElementById("orangeColor").addEventListener("click", () => {
+              vctrApi.setMaterial(object.name, orangeMaterial.name);
+            });
+
+            document.getElementById("redColor").addEventListener("click", () => {
+                vctrApi.setMaterial(object.name, redMaterial.name);
             });
 
         } catch (e) {
